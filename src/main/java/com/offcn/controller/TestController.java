1bb07257d120373114484c55936847f315cefc75
way966 <545775223@qq.com>
2021-05-11 22:05:02 +0800

    Default Changelist

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -87,7 +87,9 @@ public class TestController {
         model.addAttribute("date",new Date());
         model.addAttribute("number",123.456);
         String str = "Thymeleaf是Web和独立环境的现代服务器端Java模板引擎，能够处理HTML，XML，JavaScript，CSS甚至纯文本。\\r\\n\" + \n" +
-                "\t  \t\t\"Thymeleaf的主要目标是提供一种优雅和高度可维护的创建模板的方式。为了实现这一点，它建立在自然模板的概念上，将其逻辑注入到模板文件中，不会影响模板被用作设计原型。这改善了设计的沟通，弥补了设计和开发团队之间的差距。\\r\\n\" + \n" +
+                "\t  \t\t\"Thymeleaf的主要目标是提供一种优雅和高度可维护的创建模板的方式。为了实现这一点，它建立在自然模板的概念上，将其逻辑注入到模板文件中，" +
+                "不会影响模板被用作设计原型。这改善了设计的沟通，弥补了设计和开发团队之间的差距。" +
+                "\\r\\n\" + \n" +
                 "\t  \t\t\"Thymeleaf也从一开始就设计了Web标准 - 特别是HTML5 - 允许您创建完全验证的模板，如果这是您需要的\\r\\n\n";
         model.addAttribute("str",str);
         return "index06";
