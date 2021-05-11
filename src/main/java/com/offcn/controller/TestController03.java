9860a3251f7daba9a2a7702f4a5bf9bdf2007d82
way966 <545775223@qq.com>
2021-05-11 22:14:11 +0800

    it's the first personal change

---
+++ src/main/java/com/offcn/controller/TestController03.java
@@ -0,0 +1,14 @@
+package com.offcn.controller;
+
+/**
+ * @author: gwp
+ * @version: 1.0
+ * @date: 2021/5/11
+ * @time: 22:10
+ * @description:
+ */
+public class TestController03 {
+    public void test() {
+        System.out.println("show thymeleaf");
+    }
+}
